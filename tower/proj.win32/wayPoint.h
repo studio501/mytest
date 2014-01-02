4ae306c465089c29cb19a82d484b928bd802a28f
studio501 <inryygy007@gmail.com>
2014-01-02 09:22:02 +0800

    tower defense

---
+++ tower/proj.win32/wayPoint.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "d:\program files\cocos2d-x-2.2.1\cocos2d-x-2.2.1\cocos2dx\base_nodes\ccnode.h"
+#include "cocos2d.h"
+USING_NS_CC;
+extern class GameLayer;
+
+
+class wayPoint :
+	public CCNode
+{
+public:
+	wayPoint(void);
+	~wayPoint(void);
+	bool initWithLayer(GameLayer *gameLayer,CCPoint tempLocation);
+	void draw();
+
+public:
+	GameLayer *_gameLayer;
+	CCPoint _myPosition;
+	wayPoint *nextWayPoint;
+
+
+};
+
